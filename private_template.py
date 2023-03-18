60fa0a75c8dacfa7b08eefdf0617ce1b3fef8fee
Leo Heidelberger <you@leo.heidelberger@polytechnique.edu>
2023-03-18 18:03:03 +0000

    accessing open ai

---
+++ private_template.py
@@ -0,0 +1,8 @@
+"""
+Example of what to put in the private.py file
+"""
+import openai
+openai.organization = "TOP-SECRET-KEY"
+openai.api_key = "TOP-SECRET-KEY"
+
+assert False, 'This is a template file and should not be used'
