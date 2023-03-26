f196acd974a0e4f5d4a156c1df58cb1b3db6151f
Leo Heidelberger <you@leo.heidelberger@polytechnique.edu>
2023-03-26 10:13:54 +0100

    done skeleton

--- react_frontend/src/TimeTable.js
+++ react_frontend/src/TimeTable.js
@@ -1,24 +1,29 @@
 import React from "react";
 
-function TimeTable() {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+import './TimeTable.css';
 
-  return (
-    <div>
-      <h1>Time Table Content</h1>
-      <div className="timetable-grid">
-        {days.map((day) => (
-          <div key={day} className="day">
-            {day === "Monday" ? (
-              <div className="widget">Recipe 1</div>
-            ) : (
-              <div className="day-widget">{day}</div>
-            )}
-          </div>
-        ))}
+
+function TimeTable() {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    return (
+      <div>
+        <h1>Time Table Content</h1>
+        <div className="timetable-grid">
+          {days.map((day) => (
+            <div key={day} className="day">
+              <div className="widgets">
+                <div className="day-widget">{day}</div>
+                {day === "Monday" ? (
+                  <div className="recipe-widget">Recipe 1</div>
+                ) : null}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+
 
 export default TimeTable;
