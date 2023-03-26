c45e6565425ae05cc67127059f2ee2eb4f3f8808
Leo Heidelberger <you@leo.heidelberger@polytechnique.edu>
2023-03-26 15:39:18 +0100

    almost working

--- react_frontend/src/TimeTable.js
+++ react_frontend/src/TimeTable.js
@@ -1,84 +1,75 @@
-import React, { useState } from "react";
-import { useDrag, useDrop, DndProvider } from "react-dnd";
+import React, { useState, useCallback } from "react";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { ItemTypes } from "./ItemTypes";
+import { DndProvider } from "react-dnd";
+import update from "immutability-helper";
+import {Recipe} from './Recipe';
 
-import './TimeTable.css';
-
-const RecipeWidget = ({ name, day, onDrop }) => {
-  const [{ isDragging }, drag] = useDrag({
-    type: ItemTypes.RECIPE,
-    item: () => ({ name }),
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-    begin: (monitor) => {
-        console.log(`Started dragging ${name}`);
-    },
-  });
-
-  const [{ canDrop, isOver }, drop] = useDrop({
-    accept: ItemTypes.RECIPE,
-    drop: (item) => {
-      console.log(`Dropped ${item.name} onto ${day}`);
-      onDrop(day)(item);
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
-  });
-
-  const isActive = canDrop && isOver;
-
-  console.log(`Rendered RecipeWidget for ${name} on ${day}`);
-
-  return (
-    <div
-      ref={drop}
-      className={`recipe-widget ${isActive ? "active" : ""}`}
-      style={{ opacity: isDragging ? 0.5 : 1 }}
-    >
-      <div ref={drag} className="handle" />
-      {name}
-    </div>
-  );
-};
+import "./TimeTable.css";
 
 function TimeTable() {
-  const [recipes, setRecipes] = useState({
-    Monday: "Recipe 1",
-    Tuesday: "",
-    Wednesday: "",
-    Thursday: "",
-    Friday: "",
-    Saturday: "",
-    Sunday: "",
-  });
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
-  const onDrop = (day) => (item) => {
-    console.log(`Dropping ${item.name} onto ${day}`);
-    setRecipes((recipes) => ({
-      ...recipes,
-      [day]: item.name,
-      [Object.keys(recipes).find((key) => recipes[key] === item.name)]: "",
-    }));
-  };
+  const [recipes, setRecipes] = useState([
+    {
+        id: 1,
+        text: "Recipe 1",
+        day: "Monday",
+      },
+      {
+        id: 2,
+        text: "Recipe 2",
+        day: "Monday",
+      },
+      {
+        id: 3,
+        text: "Recipe 3",
+        day: "Tuesday",
+      },
+    ]);
 
-  console.log("Rendered TimeTable");
+  const moveRecipe = useCallback((dragIndex, hoverIndex) => {
+    setRecipes((prevRecipes) =>
+      update(prevRecipes, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, prevRecipes[dragIndex]],
+        ],
+      }),
+    );
+  }, []);
+
+  const renderRecipe = useCallback((recipe, index) => {
+    return (
+      <Recipe
+        key={recipe.id}
+        index={index}
+        id={recipe.id}
+        text={recipe.text}
+        moveRecipe={moveRecipe}
+      />
+    );
+  }, []);
 
   return (
     <DndProvider backend={HTML5Backend}>
       <div>
         <h1>Time Table Content</h1>
         <div className="timetable-grid">
-          {Object.keys(recipes).map((day) => (
+          {days.map((day) => (
             <div key={day} className="day">
               <div className="widgets">
                 <div className="day-widget">{day}</div>
-                {recipes[day] !== "" ? (
-                  <RecipeWidget name={recipes[day]} day={day} onDrop={onDrop} />
-                ) : null}
+                {recipes
+                  .filter((recipe) => recipe.day === day)
+                  .map((recipe, i) => renderRecipe(recipe, i))}
               </div>
             </div>
           ))}
