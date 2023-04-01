b8811dd0300e6ada8d1227add6bb20dd234f20cb
Leo Heidelberger <you@leo.heidelberger@polytechnique.edu>
2023-04-01 09:35:39 +0100

    cleaned project

--- gpt_utils/describe_dir.py
+++ gpt_utils/describe_dir.py
@@ -9,55 +9,60 @@ IGNORED = [
     '__init__.py',
     '__pycache__',
 ]
+SPECIFIC_DIRECTORIES = [
+    'migrations',
+    'public',
+    ]
+
+
+def should_ignore(file, startpath, gitignore_patterns, gitignore_file):
+    if file.startswith('.'):
+        return True
+    if any(fnmatch.fnmatch(file, p) for p in gitignore_patterns) or file in IGNORED:
+        return True
+    file_path = os.path.join(startpath, file)
+    if os.path.isfile(gitignore_file):
+        return any(fnmatch.fnmatch(file_path, os.path.join(startpath, p)) for p in gitignore_patterns)
+    return False
+
+def build_tree_string(contents, startpath, prefix, is_last, gitignore_patterns):
+    tree = ""
+    for i, file in enumerate(contents):
+        is_last = i == len(contents) - 1
+        file_path = os.path.join(startpath, file)
+
+        if os.path.isdir(file_path):
+            if file in SPECIFIC_DIRECTORIES:
+                tree += prefix + ("└── " if is_last else "├── ") + file + "/...\n"
+            else:
+                tree += get_directory_tree(file_path, prefix, is_last, gitignore_patterns)
+        else:
+            tree += prefix + ("└── " if is_last else "├── ") + file + "\n"
+    return tree
 
 def get_directory_tree(startpath, prefix="", is_last=False, gitignore_patterns=None):
-    """
-    Recursively returns the directory tree starting from the given startpath, ignoring files and directories that would be ignored by gitignore, as well as those starting with a dot.
-    """
     if gitignore_patterns is None:
         gitignore_patterns = []
 
     tree = ""
     if os.path.isdir(startpath):
         dirname = os.path.basename(startpath)
-        if prefix == "":
-            tree += dirname + "/\n"
-        else:
-            tree += prefix + ("└── " if is_last else "├── ") + dirname + "/\n"
+        tree += prefix + ("└── " if is_last else "├── ") + dirname + "/\n" if prefix else dirname + "/\n"
         prefix += "    " if is_last else "│   "
 
-        # get the gitignore patterns for the current directory
         gitignore_file = os.path.join(startpath, '.gitignore')
         if os.path.isfile(gitignore_file):
             with open(gitignore_file, 'r') as f:
-                for line in f:
-                    pattern = line.strip()
-                    if pattern and not pattern.startswith('#'):
-                        gitignore_patterns.append(pattern)
+                gitignore_patterns += [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
         contents = os.listdir(startpath)
-        for i, file in enumerate(contents):
-            is_last = i == len(contents) - 1
-            file_path = os.path.join(startpath, file)
+        contents = [file for file in contents if not should_ignore(file, startpath, gitignore_patterns, gitignore_file)]
 
-            # check if the file should be ignored
-            should_ignore = False
-            if file.startswith('.') or any(fnmatch.fnmatch(file, p) for p in gitignore_patterns) or file in IGNORED:
-                should_ignore = True
-            elif os.path.isdir(file_path):
-                should_ignore = False
-            elif os.path.isfile(gitignore_file):
-                should_ignore = any(fnmatch.fnmatch(file_path, os.path.join(startpath, p)) for p in gitignore_patterns)
-
-            if should_ignore:
-                continue
-
-            if os.path.isdir(file_path):
-                tree += get_directory_tree(file_path, prefix, is_last, gitignore_patterns)
-            else:
-                tree += prefix + ("└── " if is_last else "├── ") + file + "\n"
+        tree += build_tree_string(contents, startpath, prefix, is_last, gitignore_patterns)
 
     return tree
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
